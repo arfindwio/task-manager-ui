3b757e86c3bc4c270abca2db3a799e6b194e4993
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2025-11-30 00:04:21 +0700

    second commit

--- src/features/tasks/tasksAPI.ts
+++ src/features/tasks/tasksAPI.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 import type { Task } from "./tasksTypes";
 
 export const getTasksAPI = async (): Promise<Task[]> => {
-  const response = await axios.get("/src/data/tasks.json");
+  const response = await axios.get("/data/tasks.json");
   return Array.isArray(response.data) ? response.data : [];
 };
